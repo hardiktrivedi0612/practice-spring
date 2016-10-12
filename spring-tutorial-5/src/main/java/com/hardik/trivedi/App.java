e9e74f47f12491ef9d5c4a569fd58373f673d0d2
hardiktrivedi0612 <hardiktrivedi0612@gmail.com>
2016-10-11 20:36:52 -0500

    Completed s02e09

--- spring-tutorial-5/src/main/java/com/hardik/trivedi/App.java
+++ spring-tutorial-5/src/main/java/com/hardik/trivedi/App.java
@@ -1,11 +1,24 @@
 package com.hardik.trivedi;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
 	public static void main(String args[]) {
-		ApplicationContext context;
-		Person person = new Person();
+		//Fetching file from project directory
+		//ApplicationContext context = new FileSystemXmlApplicationContext("/src/main/java/com/hardik/trivedi/beans/beans-config.xml");
+		
+		//Fetching file from any folder in classpath
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/hardik/trivedi/beans/beans-config.xml");
+		
+		Person person = (Person)context.getBean("person");
 		person.speak();
+//		((FileSystemXmlApplicationContext)context).close();
+		
+		((ClassPathXmlApplicationContext)context).close();
+		
+		
+		System.out.println(person);
 	}
 }
--- spring-tutorial-5/src/main/java/com/hardik/trivedi/Person.java
+++ spring-tutorial-5/src/main/java/com/hardik/trivedi/Person.java
@@ -1,7 +1,32 @@
 package com.hardik.trivedi;
 
 public class Person {
+	
+	private int id;
+	private String name;
+	
+	private String taxId;
+	
+	public Person() {
+		super();
+	}
+
+	public Person(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
+	}
+
 	public void speak() {
 		System.out.println("Hello! I'm a person.");
+	}
+
+	public void setTaxId(String taxId) {
+		this.taxId = taxId;
 	}
 }
